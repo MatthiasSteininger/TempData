4d7100b4474a8dd3ded4fa59f3bc0de7511afcda
MatthiasSteininger <matti.steininger@gamil.com>
2024-01-25 12:47:01 +0100

    Other Service Update

--- CSharpTesting/SocketServer/MainWindow.xaml.cs
+++ CSharpTesting/SocketServer/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +15,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 using WebSocketSharp;
 using WebSocketSharp.Server;
@@ -43,7 +51,8 @@ namespace SocketServer
             this.tbkMsg.Text = "Setting UP...";
             string tbxContent = this.tbxSetupString.Text;
             string setupString = "ws://" + tbxContent + ":8080";
-            Task.Run(() => {
+            Task.Run(() =>
+            {
                 try
                 {
                     if (this.wssv == null)
@@ -52,7 +61,8 @@ namespace SocketServer
                         this.wssv.AddWebSocketService<MyWebSocketBehavior>("/WebSocketRoute");
                         this.wssv.Start();
 
-                        this.Dispatcher.BeginInvoke(new Action(() => {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
                             btnSetup.Content = "ReSetup";
                         }));
                     }
@@ -67,18 +77,20 @@ namespace SocketServer
                         this.wssv.Start();
                     }
 
-                    this.Dispatcher.BeginInvoke(new Action(() => {
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
                         tbkMsg.Text = "WebSocket Setup with '" + setupString + "' - SUCCESSFULL";
                     }));
                 }
                 catch (Exception ex)
                 {
-                    this.Dispatcher.BeginInvoke(new Action(() => {
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
                         tbkMsg.Text = "TRIED WebSocket Setup with '" + setupString + "' - UNSUCCESSFULL";
                     }));
                     MessageBox.Show("There has been an Exception while Setting up the Web Socket Server!\nExcMSG: " + ex.Message);
                 }
-            }); 
+            });
         }
 
         private void btnSendToClients_Click(object sender, RoutedEventArgs e)
@@ -87,7 +99,8 @@ namespace SocketServer
             if (this.wssv != null)
             {
                 MyWebSocketBehavior.SendMessageToClients("Hello, clients!");
-            } else
+            }
+            else
             {
                 MessageBox.Show("The Socket Server is not running!");
             }
@@ -96,6 +109,45 @@ namespace SocketServer
         private void btnSetup_Click(object sender, RoutedEventArgs e)
         {
             buildSocket();
+        }
+
+        private void btnTestOtherClient_Click(object sender, RoutedEventArgs e)
+        {
+            testSetup();
+        }
+
+        private async void testSetup()
+        {
+            this.tbkMsg.Text = "Setting UP...";
+            string tbxContent = this.tbxSetupString.Text;
+            string setupString = "ws://" + tbxContent + ":8080";
+
+            ClientWebSocket clientWebSocket = new ClientWebSocket();
+
+            try
+            {
+                await clientWebSocket.ConnectAsync(new Uri(setupString), CancellationToken.None);
+
+                // Send a message
+                string message = "Hello Server";
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                await clientWebSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                // Receive a message
+                byte[] receiveBuffer = new byte[1024];
+                WebSocketReceiveResult result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                string receivedMessage = Encoding.UTF8.GetString(receiveBuffer, 0, result.Count);
+                MessageBox.Show($"Received message: {receivedMessage}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex.Message}");
+            }
+            finally
+            {
+                if (clientWebSocket.State == System.Net.WebSockets.WebSocketState.Open)
+                    await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+            }
         }
     }
 }
